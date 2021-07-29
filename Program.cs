a9961ce8cd2fd5a4dcd97d1d98fcb3c29653b658
Cesar <cesdia883@gmail.com>
2021-07-28 23:22:03 -0400

    fresh start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace personal_accounting_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
