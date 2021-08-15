eb60829004b55a98d54a9a9af3c95a695a221fe5
Cesar <cesdia883@gmail.com>
2021-08-15 10:12:14 -0400

    test script started

---
+++ src/Commands/BranchCommand.cs
@@ -0,0 +1,25 @@
+using Services;
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace Commands
+{
+    public class BranchCommand : Command
+    {
+        public BranchCommand() : base("branch", "List your current active vault branch")
+        {
+            Handler = CommandHandler.Create(() =>
+            {
+                if (VaultBranch.Account != null)
+                {
+                    Console.WriteLine($"Current Vault Branch: {VaultBranch.Account}");
+                }
+                else
+                {
+                    Console.WriteLine("No Vault Branch set");
+                }
+            });
+        }
+    }
+}
---
+++ test/tests.sh
@@ -0,0 +1,5 @@
+# Test Branch Command
+dotnet run cobblepot branch
+
+# Test Open Command
+dotnet run cobblepot open Asset:US:CapitalOne:Checking
