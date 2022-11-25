0e9bf1738aaec9d6323699f5061fd250e7a98f9b
Ces-D <cesdia883@gmail.com>
2022-11-25 17:46:27 -0500

    some commands

--- cobblepot_cli/src/chart_of_accounts_commands.rs
+++ cobblepot_cli/src/chart_of_accounts_commands.rs
@@ -1,17 +1,16 @@
-use clap::{Arg, Command, CommandFactory};
+use clap::{Arg, Command};
 
-struct ChartOfAccountsCommandFactory;
+fn create_commands() -> Vec<Command> {
+    let open_command = Command::new("open")
+        .about("Open a new Account")
+        .after_help("Open an Account in your Chart of Accounts. Provide a name, description, and the account category for this account");
 
-impl CommandFactory for ChartOfAccountsCommandFactory {
-    fn command() -> Command {
-        Command::new("open")
-            .about("Open a new Account")
-            .after_help("Open an Account in your Chart of Accounts. Provide a name, description, and the account category for this account")
-    }
-    fn command_for_update() -> Command {
-        Command::new("open")
-    }
+    let close_command = Command::new("close")
+        .about("Close an Account")
+        .after_help("Close an Account in your Chart of Accounts.");
+
+    let save_command = Command::new("save")
+        .about("Save your changes")
+        .after_help("Save your changes to the Chart of Accounts.");
+    vec![open_command, close_command, save_command]
 }
-
-// TOOD: finish these command
-// create commands for open, close, save
