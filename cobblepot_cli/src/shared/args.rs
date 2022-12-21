7d3bf8e1a82d6ae27947791eb3166d47a357d1f8
Ces-D <cesdia883@gmail.com>
2022-12-20 20:44:02 -0500

    stuff

--- cobblepot_cli/src/shared/args.rs
+++ cobblepot_cli/src/shared/args.rs
@@ -1,27 +1,47 @@
-use super::chart_account_category::ChartAccountCategory;
-use clap::{value_parser, Arg, ArgAction};
+use clap::{Arg, ArgAction, ArgMatches};
 
-pub fn account_name_arg() -> Arg {
-    Arg::new("name")
-        .short('n')
-        .long("name")
-        .help("Unique name of this account")
-        .action(ArgAction::Set)
+pub enum AcceptedArg {
+    Name,
+    Description,
+    Category,
 }
 
-pub fn account_description_arg() -> Arg {
-    Arg::new("description")
-        .short('d')
-        .long("description")
-        .help("Description of this account")
-        .action(ArgAction::Set)
+pub fn create_args(accepted: Vec<AcceptedArg>) -> Vec<Arg> {
+    let mut args: Vec<Arg> = Vec::new();
+
+    for ele in accepted {
+        let arg = match ele {
+            AcceptedArg::Name => Arg::new("name")
+                .short('n')
+                .help("Unique name of this account")
+                .action(ArgAction::Set)
+                .required(true),
+            AcceptedArg::Description => Arg::new("description")
+                .short('d')
+                .help("Description of this account")
+                .action(ArgAction::Set)
+                .required(true),
+            AcceptedArg::Category => Arg::new("category")
+                .short('c')
+                .help("Type of this account")
+                .action(ArgAction::Set)
+                .required(true),
+        };
+
+        args.push(arg);
+    }
+
+    args
 }
 
-pub fn account_category_arg() -> Arg {
-    Arg::new("category")
-        .short('c')
-        .long("category")
-        .help("Type of this account")
-        .value_parser(value_parser!(ChartAccountCategory))
-        .action(ArgAction::Set)
-}
+pub fn find_missing_arg(accepted: Vec<AcceptedArg>, matches: ArgMatches) -> Option<AcceptedArg> {}
+
+// set the standard for args and values of args
+// maybe this is passed an array of AllowedArgs and it creates the collection of Args for the
+// Command
+// This can then also get the matches and compare with the identifies allowed args
+// struct
+// construct - collection of AllowedArgs
+//  - internal generate args collection relative to contructor params
+//  - takes a ArgMatches and returns the missing values or nothing if all is input - checks for
+//  missing
